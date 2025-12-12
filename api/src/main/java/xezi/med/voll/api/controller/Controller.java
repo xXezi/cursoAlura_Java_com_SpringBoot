54ee08df12b082fa98799993b215d59512b58ccf
xXezi <willroberti04@hotmail.com>
2025-12-12 17:43:38 -0300

    hello world no spring

---
+++ api/src/main/java/xezi/med/voll/api/controller/Controller.java
@@ -0,0 +1,15 @@
+package xezi.med.voll.api.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello")
+public class Controller {
+
+	@GetMapping
+	public String helloWorld() {
+		return "Hello World";
+	}
+}
